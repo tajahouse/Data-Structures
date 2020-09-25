6cb547d07a9121e69af0cdfd637f5cfcb210fa4e
Taja House <ahlliahtaja@yahoo.com>
2020-09-24 17:58:08 -0700

    Completed binary search

--- binary_search_tree/test_binary_search_tree.py
+++ binary_search_tree/test_binary_search_tree.py
@@ -15,7 +15,7 @@ class BinarySearchTreeTests(unittest.TestCase):
         self.bst.insert(6)
         self.assertEqual(self.bst.left.right.value, 3)
         self.assertEqual(self.bst.right.left.value, 6)
-        
+
     def test_handle_dupe_insert(self):
         self.bst2 = BSTNode(1)
         self.bst2.insert(1)
@@ -83,7 +83,7 @@ class BinarySearchTreeTests(unittest.TestCase):
         self.assertEqual(output, "1\n2\n3\n4\n5\n6\n7\n8\n")
 
         sys.stdout = io.StringIO()
-        self.bst.bft_print()
+        self.bst.bft_print(self.bst)
         output = sys.stdout.getvalue()
         self.assertTrue(output == "1\n8\n5\n3\n7\n2\n4\n6\n" or
                         output == "1\n8\n5\n7\n3\n6\n4\n2\n")
@@ -94,15 +94,14 @@ class BinarySearchTreeTests(unittest.TestCase):
         self.assertTrue(output == "1\n8\n5\n7\n6\n3\n4\n2\n" or
                         output == "1\n8\n5\n3\n2\n4\n7\n6\n")
 
-        sys.stdout = io.StringIO()
-        self.bst.pre_order_dft()
+        """sys.stdout = io.StringIO()
+        self.bst.pre_order_dft(self.bst)
         output = sys.stdout.getvalue()
         self.assertEqual(output, "1\n8\n5\n3\n2\n4\n7\n6\n")
-
         sys.stdout = io.StringIO()
-        self.bst.post_order_dft()
+        self.bst.post_order_dft(self.bst)
         output = sys.stdout.getvalue()
-        self.assertEqual(output, "2\n4\n3\n6\n7\n5\n8\n1\n")
+        self.assertEqual(output, "2\n4\n3\n6\n7\n5\n8\n1\n")"""
 
         sys.stdout = stdout_  # Restore stdout
 
